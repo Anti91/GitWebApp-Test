442547399f07f34bbb5b75acebb68d69ec24dbdf
Szösz Gergő <szosz.gergo@marktsoft.hu>
2018-02-23 10:57:36 +0100

    Masodik

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        //test
         public ActionResult Index()
         {
             return View();
